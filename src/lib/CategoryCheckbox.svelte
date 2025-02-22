869551aa6b223f397da46adf8dc84444116866c8
chun <chun@nowhere>
2025-02-22 01:27:34 -0500

    Preliminary checkbox

---
+++ src/lib/CategoryCheckbox.svelte
@@ -0,0 +1,15 @@
+<script>
+    let { categories } = $props();
+</script>
+
+<div>
+    {#each Object.entries(categories) as [category, games]}
+    <h1>{category}</h1>
+        {#each games as game}
+        <div>
+            <input type="checkbox" id="cbox_{game}" />
+            <label for="cbox_{game}">{game}</label>
+        </div>
+        {/each}
+    {/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import CategoryCheckbox from "$lib/CategoryCheckbox.svelte";
+    let categoriesList = {
+        "FPS": [
+            "Call of Duty",
+            "Bioshock"
+        ],
+        "RPG": [
+            "Final Fantasy",
+            "Mass Effect",
+            "Baldur's Gate"
+        ]
+    };
+</script>
+
+<h1>wtf?</h1>
+<CategoryCheckbox categories={categoriesList} />
